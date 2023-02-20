659181d759370727bc3c14f4473d4fbbe66a27de
Chizoba Chukwuoma <45069751+classychizzy@users.noreply.github.com>
2023-02-20 23:02:49 +0100

    changed line endings

---
+++ 0x05-Node_js_basic/0-console.js
@@ -0,0 +1,6 @@
+// a function that displays a message
+function displayMessage(str) {
+	console.log(str)
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_js_basic/README.md
@@ -0,0 +1,1 @@
+# nodejs project with tests using Mocha
