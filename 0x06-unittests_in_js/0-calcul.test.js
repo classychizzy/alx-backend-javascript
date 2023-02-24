267cf18c2c6a94f0da658523203383266e487b7b
classychizzy <chizobachukwuoma@gmail.com>
2023-02-24 20:42:40 +0000

    changed expect to assert

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,14 +1,14 @@
 /* test for the calculateNumber function
  */
 const calculateNumber = require('./0-calcul');
-const expect = require('chai').expect;
+const assert = require('chai').assert;
 
 describe('calculateNumber', function () {
 	it('rounds a fractional number and calculates the sum', () => {
-		expect(calculateNumber(1.5, 2.3)).equal(5);
+		assert.strictEqual(calculateNumber(1.5, 2.3), 5);
 	});
 
 	it('rounds a floating number and calculates the sum', () => {
-		expect(calculateNumber(1.0, 2.0)).equal(3);
+		assert.strictEqual(calculateNumber(1.0, 2.0), 3);
 	});
 });
